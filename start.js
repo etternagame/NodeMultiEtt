60295d15f532c6c43c909fbf9bad95a79532eb50
theropfather <theropfather@gmail.com>
2018-06-13 16:53:14 +0100

    Remove default MongoDB URI
    This is now required to start, so applying a default isn't needed and the default of undefined only causes errors.

--- start.js
+++ start.js
@@ -30,7 +30,7 @@ if ('help' in argv || !('MONGODB_URI' in argv)) {
 const DB_NAME = argv.DB_NAME || process.env.DB_NAME || 'ettmulti';
 const PORT = argv.PORT || process.env.PORT || 8765;
 const LOGPACKETS = argv.LOG_PACKETS || process.env.LOG_PACKETS || true;
-const MONGODB_URI = argv.MONGODB_URI || process.env.MONGODB_URI || '';
+const MONGODB_URI = argv.MONGODB_URI || process.env.MONGODB_URI;
 const PING_INTERVAL = argv.PING_INTERVAL || process.env.PING_INTERVAL || 15000;
 const BOT_TOKEN = argv.BOT_TOKEN || process.env.BOT_TOKEN || '';
 
