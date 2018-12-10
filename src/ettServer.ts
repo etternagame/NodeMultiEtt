da03478de8b9b0cef40cb7794dbc15bdd5ea0d35
TheROPFather <theropfather@gmail.com>
2018-12-10 02:58:42 +0000

    Update to 256 to match EO.

--- src/ettServer.ts
+++ src/ettServer.ts
@@ -712,7 +712,7 @@ export class ETTServer {
       return;
     }
 
-    const maxLength = 128;
+    const maxLength = 256;
     const minLength = 2;
     const passLength = message.pass.length;
     const userLength = message.user.length;
