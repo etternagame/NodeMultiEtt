7b6504e381aae3b1e154a627908815724a423979
theropfather <theropfather@gmail.com>
2018-12-16 07:14:21 +0000

    Remove redundant files.

--- src/export.ts
+++
@@ -1,6 +0,0 @@
-// / <reference path="references.ts" />
-import { ETTServer } from './ettServer';
-
-module.exports = {
-  Server: ETTServer
-};
--- src/references.ts
+++
@@ -1,5 +0,0 @@
-// / <reference path="player.ts" />
-// / <reference path="room.ts" />
-// / <reference path="chart.ts" />
-// / <reference path="messages.ts" />
-// / <reference path="ettServer.ts" />
